9ae8102e07e06f3d95ba5018618cf6589641796f
Andy Tran <andyhqtran@gmail.com>
2015-07-28 13:28:59 -0700

    add in paper dialog test

--- package.js
+++ package.js
@@ -46,6 +46,10 @@ Package.onUse(function(api) {
     'lib/paper-elements/PaperRipple/PaperRipple.tpl.jade',
     'lib/paper-elements/PaperRipple/PaperRipple.es6',
 
+    'lib/paper-elements/PaperDialog/PaperDialog.scss',
+    'lib/paper-elements/PaperDialog/PaperDialog.tpl.jade',
+    'lib/paper-elements/PaperDialog/PaperDialog.es6',
+
     'lib/paper-elements/PaperCheckbox/PaperCheckbox.scss',
     'lib/paper-elements/PaperCheckbox/PaperCheckbox.tpl.jade',
     'lib/paper-elements/PaperCheckbox/PaperCheckbox.es6',
