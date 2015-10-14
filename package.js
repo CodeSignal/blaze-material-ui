7e5fb379a9c0278c57fd5d8b250f4d71ba070bb0
Gregor Adams <greg@codefights.com>
2015-10-14 15:28:04 -0700

    fix selector and add new version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'codefights:blaze-material-ui',
-  version: '0.0.2',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
   summary: '',
   // URL to the Git repository containing the source code for this package.
