f95e42f8156bdf7fd118577b69a1bc488e1c5df0
Gregor Adams <greg@codefights.com>
2015-10-17 09:48:20 -0700

    fix typo

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'codefights:blaze-material-ui',
-  version: '0.0.3',
+  version: '0.0.4',
   // Brief, one-line summary of the package.
   summary: '',
   // URL to the Git repository containing the source code for this package.
