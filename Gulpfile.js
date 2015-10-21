b87c1f91eca82a7f562fbe987817c5717b568196
Gregor Adams <greg@codefights.com>
2015-10-21 11:28:12 -0700

    fix some version inconsistencies and remove deprecated gulp stuff

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,23 +1,8 @@
 var gulp = require('gulp');
-var wrap = require('gulp-wrap');
-var concat = require('gulp-concat-sourcemap');
 var argv = require('yargs').argv;
 var template = require('gulp-template');
-var data = require('gulp-data');
-var replace = require('gulp-replace');
 var rename = require("gulp-rename");
-var del = require('del');
-var path = require('path');
 
-var paths = {
-  scripts: ['lib/**/*.es6'],
-  styles: ['lib/**/*.scss'],
-  templates: ['lib/**/*.tpl.jade']
-};
-
-gulp.task('clean', function(cb) {
-  del(['./dist'], cb);
-});
 
 gulp.task('component', [], function() {
   return gulp.src('gulp/template/*')
@@ -33,37 +18,5 @@ gulp.task('component', [], function() {
     }))
     .pipe(gulp.dest('./lib/'+ argv.type));
 });
-
-gulp.task('scripts', ['clean'], function() {
-  return gulp.src(paths.scripts)
-    .pipe(concat('blaze-material-ui.es6'))
-    .pipe(gulp.dest('./dist'));
-});
-
-gulp.task('styles', ['clean'], function() {
-  return gulp.src(paths.styles)
-    .pipe(concat('blaze-material-ui.scss'))
-    .pipe(gulp.dest('./dist'));
-});
-
-gulp.task('templates', ['clean'], function() {
-  return gulp.src(paths.templates)
-    .pipe(replace(/(.*)/ig, '  $1'))
-    .pipe(replace(/  \n/ig, '\n'))
-    .pipe(replace(/ \n/ig, '\n'))
-    .pipe(replace(/\n+/ig, '\n'))
-    .pipe(replace(/  $/ig, ''))
-    .pipe(data(function(file) {
-       var name = path.basename(file.path).replace('.tpl.jade','');
-       return {name: name}
-     }))
-    .pipe(wrap('template(name="<%= data.name %>")\n<%= data.contents %>',{}, {variable: 'data'}))
-    .pipe(concat('blaze-material-ui.jade'))
-    .pipe(gulp.dest('./dist'));
-});
-
-
-gulp.task('default', [ 'scripts', 'styles', 'templates']);
-
 
 
