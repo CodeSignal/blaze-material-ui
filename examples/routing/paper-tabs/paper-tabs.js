ade2d482f67daa19d33d78c803e3336e3a98535e
aram <aramshat@gmail.com>
2016-04-09 12:27:09 +0400

    Removed examples folder

--- examples/routing/paper-tabs/paper-tabs.js
+++
@@ -1,9 +0,0 @@
-if (Meteor.isClient) {
-  Template.paperTabs.events(
-  {
-    'click [event-hook=tabEvent]': function(e,instance){
-      console.log('>>>\nclicked tab on: ', instance,'\n<<<')
-    }
-  }
-);
-}
